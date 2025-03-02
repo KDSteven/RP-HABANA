fcf278c92ec2b9421a11512640f41f27bd8a78a0
Kent Steven <kdperez@ccc.edu.ph>
2025-03-02 17:37:34 +0800

    4th commit

--- index.php
+++ index.php
@@ -6,9 +6,7 @@
     <title>R.P Habana - Inventory System</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
-
-        /* asdsad    */
-
+        
         body {
             height: 100vh;
         }
