5c31be232e0526ce5a873a9429b9ca7d9e8f5de6
JeArhax <cooljohnric24@gmail.com>
2025-09-06 20:27:53 +0800

    lapit na ser

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 $servername = "localhost";  
 $username = "root";         
 $password = "";            
-$dbname = "rp habana";  
+$dbname = "rp_habana";  
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
